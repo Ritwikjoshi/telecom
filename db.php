e59cee358a7c0538579f2c3193716bcbfbd35a19
Ritwik Joshi <jritwik3@gmail.com>
2017-03-28 13:42:11 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,16 @@
 <?php
-	$connection = mysql_connect('localhost', 'root', '');
+
+    $db = mysqli_init();
+    $db->ssl_set( "telecom/certificates/5e4da5640f0d5a-key.pem", "telecom/certificates/5e4da5640f0d5a-cert.pem ", " telecom/certificates/cleardb-ca.pem ", null, null);
+    $connection->real_connect("127.0.0.1:22", "ritwik", "", "telecom");
+
 	if (!$connection)
 	{
-		die("Database Connection Failed" . mysql_error());
+		die("Database Connection Failed" . mysqli_error($connection));
 	}
-	$select_db = mysql_select_db('register');
+	$select_db = mysqli_select_db('register');
 	if (!$select_db)
 	{
-		die("Database Selection Failed" . mysql_error());
+		die("Database Selection Failed" . mysqli_error($connection));
 	}
 ?>
